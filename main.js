481a5cb3dccc18832f7b9e4e59bd6da2db49bbfa
TomasMarioBR <164898640+TomasMarioBR@users.noreply.github.com>
2024-04-10 11:27:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function somar() {
+    var num1 = document.getElementById
+}
+
+ function somar() {
+    var num1 = document.getElementById("n1").value;
+    var num2 = document.getElementById("n2").value;
+    var resultado = document.getElementById("res");
+    num1 = Number(num1)
+    num2 = Number(num2)
+    resu = num1 + num2
+    resultado.innerHTML = (`O resultado da soma entre ${num1} + ${num2} e igual a:  ${resu}`)
+
+    //aula 89 revisão//
+    var nome = window.prompt ('Qual seu nome?') //var guarda o nome
+    document.write(`Ola ${nome} seu nome tem ${nome.length} letras <br>`)
+
+    document.write (`Seu nome em maiusculo é ${nome.toLocaleUpperCase} <br>`) 
+    document.write(`Seu nome em minusculo é ${nome.toLocaleUpperCase} <br> `)
+
+
+
+
+    
+ }
